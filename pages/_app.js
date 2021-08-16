9f417bcec4107188e98a8f1b48a48785eff83db4
Yvens16 <yvensbelaston@gmail.com>
2021-08-16 16:09:22 +0200

    facebook domain verification tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 // import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
 import Layout from '../components/Layout';
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <meta name="facebook-domain-verification" content="d9atyj6cfztltzm5512z4bpvhk7uz7" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
